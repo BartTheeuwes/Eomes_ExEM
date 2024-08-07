a1aa9c59f96a71ea578b2ff76725ff4d0e9766d2
BartTheeuwes <91329477+BartTheeuwes@users.noreply.github.com>
2024-08-07 14:41:44 +0100

    Chimera-seq code

---
+++ chimera_seq/mapping/run/parse_sample_metadata_after_mapping.R
@@ -0,0 +1,63 @@
+suppressPackageStartupMessages(library(argparse))
+suppressPackageStartupMessages(library(SingleCellExperiment))
+suppressPackageStartupMessages(library(data.table))
+
+here::i_am("mapping/run/parse_sample_metadata_after_mapping.R")
+source(here::here("settings.R"))
+
+######################
+## Define arguments ##
+######################
+
+p <- ArgumentParser(description='')
+# p$add_argument('--query_samples',   type="character",   nargs='+',  help='Query samples')
+p$add_argument('--metadata',    type="character",  help='Metadata file to use as input')
+p$add_argument('--mapping_mnn',    type="character",  nargs="+", help='Results of the MNN mapping')
+p$add_argument('--outfile',          type="character",               help='Output file')
+p$add_argument('--sce',         type="character",                            help='SingleCellExperiment file')
+args <- p$parse_args(commandArgs(TRUE))
+
+###################
+## Load settings ##
+###################
+
+
+## START TEST ##
+# args$query_samples <- opts$samples
+# args$metadata <- file.path(io$basedir,"results/rna/qc/sample_metadata_after_qc.txt.gz")
+# # args$mapping_dir <- file.path(io$basedir,"results/rna/mapping")
+# args$mapping_mnn <- file.path(io$basedir,"results/rna/mapping(..)")
+# args$mapping_seurat <- file.path(io$basedir,"results/rna/mapping/(..)")
+# args$outfile <- file.path(io$basedir,"results/rna/mapping/sample_metadata_after_mapping.txt.gz")
+## END TEST ##
+
+
+###################
+## Load metadata ##
+###################
+
+sample_metadata <- fread(args$metadata)
+
+##########################
+## Load mapping results ##
+##########################
+
+mapping_mnn.dt <- args$mapping_mnn %>% map(~ fread(.)) %>% rbindlist
+
+
+stopifnot(mapping_mnn.dt$cell%in%sample_metadata$cell)
+
+###########
+## Merge ##
+###########
+
+colnames(mapping_mnn.dt) = c("cell","celltype.mapped_mnn","celltype.score_mnn","celltype_extended.mapped_mnn","celltype_extended.score_mnn","stage.mapped_mnn", "cellstage.score_mnn", "closest.cell_mnn")
+
+to.save <- sample_metadata %>% 
+  merge(mapping_mnn.dt,by="cell",all.x=TRUE)
+
+#################
+## Save output ##
+#################
+
+fwrite(to.save, args$outfile, sep="\t", na="NA", quote=F)
